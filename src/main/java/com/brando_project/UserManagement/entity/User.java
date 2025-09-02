f5a8767463398a889f258072e1013209c6193164
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-09-02 20:41:03 -0300

    entity class created

---
+++ src/main/java/com/brando_project/UserManagement/entity/User.java
@@ -0,0 +1,32 @@
+package com.brando_project.UserManagement.entity;
+
+import org.springframework.boot.context.properties.bind.Name;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "idade", nullable = false)
+    private int idade;
+
+}
