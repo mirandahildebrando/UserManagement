d4111f134291b15d121ea933cbe2f2b38c491b49
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-09-02 20:30:09 -0300

    created project structure

---
+++ src/test/java/com/brando_project/UserManagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brando_project.UserManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
